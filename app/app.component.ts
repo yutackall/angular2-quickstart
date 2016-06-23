8b1217da20683b4883cd36c93366a79331eae4ff
yutackall <yutackall@gmail.com>
2016-06-23 15:47:49 +0900

    Replace the <input>

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,16 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template:`
-      <h1>{{title}}</h1>
-      <h2>{{hero.name}} details!</h2>
-      <div><label>id: </label>{{hero.id}}</div>
-      <div>
-        <label>name: </label>
-        <input value="{{hero.name}}" placeholder="name">
-      </div>
-      `
+  selector: 'my-app',
+  template:`
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
 
 })
 export class AppComponent {
